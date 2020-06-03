de94315927d7a9c72c8f0a2228c21fc3a6bd9cce
Gene-Dana <yllanesgene@gmail.com>
2020-06-03 13:34:49 -0400

    changed to simple table

--- src/components/QuoteForm.vue
+++ src/components/QuoteForm.vue
@@ -13,182 +13,197 @@
       @submit.prevent="handleSubmit2"
     >
       <input type="hidden" name="name" />
-
       <input type="hidden" name="email" />
-
       <input type="hidden" name="number" />
+
+      <input type="hidden" name="type" />
+      <input type="hidden" name="color" />
 
       <input type="hidden" name="name_0" />
       <input type="hidden" name="height_0" />
       <input type="hidden" name="width_0" />
-      <input type="hidden" name="type_0" />
-      <input type="hidden" name="color_0" />
 
       <input type="hidden" name="name_1" />
       <input type="hidden" name="height_1" />
       <input type="hidden" name="width_1" />
-      <input type="hidden" name="type_1" />
-      <input type="hidden" name="color_1" />
 
       <input type="hidden" name="name_2" />
       <input type="hidden" name="height_2" />
       <input type="hidden" name="width_2" />
-      <input type="hidden" name="type_2" />
-      <input type="hidden" name="color_2" />
 
       <input type="hidden" name="name_3" />
       <input type="hidden" name="height_3" />
       <input type="hidden" name="width_3" />
-      <input type="hidden" name="type_3" />
-      <input type="hidden" name="color_3" />
 
       <input type="hidden" name="name_4" />
       <input type="hidden" name="height_4" />
       <input type="hidden" name="width_4" />
-      <input type="hidden" name="type_4" />
-      <input type="hidden" name="color_4" />
 
       <input type="hidden" name="name_5" />
       <input type="hidden" name="height_5" />
       <input type="hidden" name="width_5" />
-      <input type="hidden" name="type_5" />
-      <input type="hidden" name="color_5" />
 
       <input type="hidden" name="name_6" />
       <input type="hidden" name="height_6" />
       <input type="hidden" name="width_6" />
-      <input type="hidden" name="type_6" />
-      <input type="hidden" name="color_6" />
 
       <input type="hidden" name="name_7" />
       <input type="hidden" name="height_7" />
       <input type="hidden" name="width_7" />
-      <input type="hidden" name="type_7" />
-      <input type="hidden" name="color_7" />
 
       <input type="hidden" name="name_8" />
       <input type="hidden" name="height_8" />
       <input type="hidden" name="width_8" />
-      <input type="hidden" name="type_8" />
-      <input type="hidden" name="color_8" />
 
       <input type="hidden" name="name_9" />
       <input type="hidden" name="height_9" />
       <input type="hidden" name="width_9" />
-      <input type="hidden" name="type_9" />
-      <input type="hidden" name="color_9" />
 
       <v-card class="pa-3">
-        <v-data-table
-          :headers="headers"
-          :items="shutters"
-          hide-default-header
-          hide-default-footer
-          class="elevation-1"
-        >
-          <template v-slot:top></template>
-          <template v-slot:item.action="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
-            <v-icon small @click="deleteItem(item)">delete</v-icon>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Type</th>
+                <th class="text-left">Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <v-select :items="types" name="type"></v-select>
+                </td>
+                <td>
+                  <v-select :items="colors" name="colors"></v-select>
+                </td>
+              </tr>
+            </tbody>
           </template>
-          <template v-slot:no-data>
-            <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </v-simple-table>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Name</th>
+                <th class="text-left">Height</th>
+                <th class="text-left">Width</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[0].name" name="name_0"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[0].height" name="height_0"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[0].width" name="width_0"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[1].name" name="name_1"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[1].height" name="height_1"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[1].width" name="width_1"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[2].name" name="name_2"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[2].height" name="height_2"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[2].width" name="width_2"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[3].name" name="name_3"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[3].height" name="height_3"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[3].width" name="width_3"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[4].name" name="name_4"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[4].height" name="height_4"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[4].width" name="width_4"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[5].name" name="name_5"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[5].height" name="height_5"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[5].width" name="width_5"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[6].name" name="name_6"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[6].height" name="height_6"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[6].width" name="width_6"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[7].name" name="name_7"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[7].height" name="height_7"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[7].width" name="width_7"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[8].name" name="name_8"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[8].height" name="height_8"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[8].width" name="width_8"></v-text-field>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <v-text-field v-model="shutters[9].name" name="name_9"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[9].height" name="height_9"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="shutters[9].width" name="width_9"></v-text-field>
+                </td>
+              </tr>
+            </tbody>
           </template>
-          <v-toolbar flat color="white">
-            <v-dialog v-model="dialog" max-width="500px">
-              <template v-slot:activator="{ on }">
-                <v-btn color="primary" dark class="mb-2" v-on="on">New Shutter</v-btn>
-              </template>
-
-              <v-card>
-                <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="12" md="12">
-                        <v-text-field
-                          v-model="editedItem.name"
-                          name="opening_name_1"
-                          label="Opening Name"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="12" md="12">
-                        <v-text-field v-model="editedItem.width" name="width_2" label="Width"></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="12" md="12">
-                        <v-text-field v-model="editedItem.height" name="height_1" label="Height"></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="12" md="12">
-                        <v-select
-                          :items="types"
-                          v-model="editedItem.type"
-                          name="type_1"
-                          label="Shutter Type"
-                        ></v-select>
-                      </v-col>
-
-                      <v-col cols="12" sm="12" md="12">
-                        <v-text-field v-model="editedItem.color" name="color_1" label="Color"></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                  <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
-        </v-data-table>
-
-        <v-toolbar flat color="white" row>
-          <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="{ on }">
-              <v-btn width="10em" color="primary" dark class v-on="on">Add Shutter</v-btn>
-            </template>
-
-            <v-card>
-              <v-card-title>
-                <span class="headline">{{ formTitle }}</span>
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="12" md="12">
-                      <v-text-field v-model="editedItem.name" label="Opening Name"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="12" md="12">
-                      <v-text-field v-model="editedItem.width" label="Width"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="12" md="12">
-                      <v-text-field v-model="editedItem.height" label="Height"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="12" md="12">
-                      <v-select :items="types" v-model="editedItem.type" label="Shutter Type"></v-select>
-                    </v-col>
-
-                    <v-col cols="12" sm="12" md="12">
-                      <v-text-field v-model="editedItem.color" label="Color"></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-
-          <v-spacer></v-spacer>
-        </v-toolbar>
+        </v-simple-table>
+        <!-- ========================================================================== -->
       </v-card>
       <v-layout row wrap justify-center align-center class="pa-3">
         <v-flex xs12 sm12 md4 lg4>
@@ -221,7 +236,7 @@
             required
           ></v-text-field>
         </v-flex>
-        <v-btn width="10em" color="success" type="submit" @click.prevent="handleSubmit2" >Get Quote</v-btn>
+        <v-btn width="10em" color="success" type="submit" @click.prevent="handleSubmit2">Get Quote</v-btn>
       </v-layout>
     </form>
   </v-container>
@@ -236,11 +251,54 @@ export default {
     initialize() {
       this.shutters = [
         {
-          name: "Example Door",
+          name: "eg.. Door",
           height: "96",
-          width: "36",
-          type: ".050 Aluminum",
-          color: "White"
+          width: "36"
+        },
+        {
+          name: "eg.. Window 1",
+          height: "40",
+          width: "50"
+        },
+        {
+          name: "",
+          height: "",
+          width: ""
+        },
+        {
+          name: "",
+          height: "",
+          width: ""
+        },
+        {
+          name: "",
+          height: "",
+          width: ""
+        },
+        {
+          name: "",
+          height: "",
+          width: ""
+        },
+        {
+          name: "",
+          height: "",
+          width: ""
+        },
+        {
+          name: "",
+          height: "",
+          width: ""
+        },
+        {
+          name: "",
+          height: "",
+          width: ""
+        },
+        {
+          name: "",
+          height: "",
+          width: ""
         }
       ];
     },
@@ -537,6 +595,7 @@ export default {
         "24G Steel Panels",
         "Roll Down"
       ],
+      colors: ["White", "Ivory", "Beige", "Brown"],
       dialog: false,
       headers: [
         {
