555f9c87a4bb6415b0371a5b85be10574c3f9181
Gene <45580241+Gene-Dana@users.noreply.github.com>
2020-08-28 15:25:16 -0400

    added CC

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,22 @@
 <template>
-	<v-footer color="red" padless>
-		<v-row justify="center" no-gutters>
-			<v-btn to="/home" color="white" text rounded class="my-2">Home</v-btn>
-			<v-btn to="/how to measure" color="white" text rounded class="my-2">How To Measure</v-btn>
-			<v-btn to="/Free Quote" color="white" text rounded class="my-2">Free Quote</v-btn>
-			<v-col class="red py-4 text-center white--text" cols="12">
-				{{ new Date().getFullYear() }} —
-				<strong>Supreme Hurricane Shutters</strong>
-			</v-col>
-		</v-row>
-	</v-footer>
+  <v-footer color="red" padless>
+    <v-row justify="center" no-gutters>
+      <v-btn to="/home" color="white" text rounded class="my-2">Home</v-btn>
+      <v-btn to="/how to measure" color="white" text rounded class="my-2">How To Measure</v-btn>
+      <v-btn to="/Free Quote" color="white" text rounded class="my-2">Free Quote</v-btn>
+      <v-col class="red py-4 text-center white--text" cols="12">
+        {{ new Date().getFullYear() }} —
+        <strong>Supreme Hurricane Shutters CC# 20-AL-21960-X </strong>
+        
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
 
 <script>
 export default {
-	data: () => ({
-		links: ["Home", "About Us", "Free Quote"]
-	})
+  data: () => ({
+    links: ["Home", "About Us", "Free Quote"],
+  }),
 };
 </script>
